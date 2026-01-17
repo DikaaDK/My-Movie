076f02e1c7ce35d86f6f6937e69f29e3bd484fe1
Dika Muzacky <dikayla08@gmail.com>
2026-01-17 23:58:50 +0700

    cleaning part 3

--- tmp_line_numbers.ps1
+++
@@ -1,7 +0,0 @@
-$counter = 0
-Get-Content 'lib/services/anime_api.dart' | ForEach-Object {
-  $counter++
-  if ($counter -le 220) {
-    Write-Output ("{0}: {1}" -f $counter, $_)
-  }
-}
--- tmp_print_lines.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  final lines = File('lib/pages/explore_page.dart').readAsLinesSync();
-  for (var i = 280; i < 340 && i < lines.length; i++) {
-    print('${i + 1}: ${lines[i]}');
-  }
-}
